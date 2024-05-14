7c954a9c3625847d9663c0adb74294303682e66d
Lucas GC <lgc@certi.org.br>
2024-05-14 15:37:26 -0300

    build: Add entrypoint for docker

---
+++ scripts/run-entrypoint.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+#/*******************************************************************************
+# * Copyright (c) 2024 - Fundação CERTI
+# * All rights reserved.
+# *******************************************************************************/
+set -e
+/code/scripts/steve-certi-build.sh 
+/code/scripts/steve-certi-run.sh
