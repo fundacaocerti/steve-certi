398151a8554e99e029dc43a2f13835d2e93cf5b8
Lucas GC <lgc@certi.org.br>
2024-06-06 18:17:37 -0300

    revert: Charging Profile Controller

--- src/main/java/de/rwth/idsg/steve/web/api/ChargingProfileController.java
+++ src/main/java/de/rwth/idsg/steve/web/api/ChargingProfileController.java
@@ -34,16 +34,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import de.rwth.idsg.steve.service.ChargePointService16_Client;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import de.rwth.idsg.steve.web.dto.ChargingProfileForm;
 import org.springframework.http.ResponseEntity;
-import de.rwth.idsg.steve.web.dto.ocpp.ClearChargingProfileParams;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 
 import java.util.HashMap;
 import java.util.Map;
