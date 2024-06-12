bcf0fdaa3533c396a72fddf5a286e03629de3301
Lucas GC <lgc@certi.org.br>
2024-06-12 14:05:00 -0300

    build: Ask git for project root folder before running

--- scripts/run-webserver.sh
+++ scripts/run-webserver.sh
@@ -1,5 +1,6 @@
 #/bin/bash
+PROJECT_ROOT="$(git rev-parse --show-toplevel)"
 git submodule init
 git submodule update
-docker compose -f ../docker-compose-production.yml down
-docker compose -f ../docker-compose-production.yml up --build
+docker compose -f $PROJECT_ROOT/docker-compose-production.yml down
+docker compose -f $PROJECT_ROOT/docker-compose-production.yml up --build
