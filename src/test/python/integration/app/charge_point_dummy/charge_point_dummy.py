a5efd44a28e20d91bf79b7aa35aecf598c8844dd
Eduardo Rodrigues de Abreu <erd@certi.org.br>
2024-06-17 11:07:51 -0300

    Add MeterValues.req to the charge point dummy

--- src/test/python/integration/app/charge_point_dummy/charge_point_dummy.py
+++ src/test/python/integration/app/charge_point_dummy/charge_point_dummy.py
@@ -8,20 +8,30 @@ import time
 import uuid
 import threading
 import websocket
+import datetime
 
 from v16.payload import (
     BootNotificationPayload,
     StatusNotificationPayload,
     HeartbeatPayload,
     ClearChargingProfilePayload,
-    SetChargingProfilePayload
+    SetChargingProfilePayload,
+    MeterValuesPayload,
+    SampledValue,
+    MeterValue
 )
 
 from v16.enums import (
     Action,
     ChargePointStatus,
     ChargePointErrorCode,
-    ClearChargingProfileStatus
+    ClearChargingProfileStatus,
+    ReadingContext,
+    ValueFormat,
+    Measurand,
+    Phase,
+    Location,
+    Unit
 )
 
 from protocol.websocket import WebSocketHelper
@@ -149,6 +159,9 @@ class ChargePointDummy:
 
         return '[{},"{}",{}]'.format(MESSAGE_TYPE_NUMBER, uuid, payload)
 
+    def timestamp(self) -> None:
+        return time.strftime("%Y-%m-%dT%H:%M:%SZ", time.gmtime())
+
     ##
     # Direction: Client-to-Server
 
@@ -201,6 +214,123 @@ class ChargePointDummy:
 
         call = self.create_call_message(Action.STATUS_NOTIFICATION.value,
                                         payload.to_json())
+
+        self.__ws.send(call)
+
+        self.__ws.receive()
+
+    def start_transaction_req(self, connector_id, id_tag) -> int:
+        '''
+        This method is responsible for inform about a transaction that has been started.
+
+        @param connector_id This contains a number (>0) designating a connector of the
+        Charge Point.
+        @param id_tag This contains the identifier for which a transaction has to be started.
+        @return This contains the transaction id supplied by the Central System.
+        '''
+        payload = StartTransactionPayload(
+            connector_id=connector_id,
+            id_tag=id_tag,
+            meter_start=0,
+            timestamp=self.timestamp()
+        )
+
+        call = self.create_call_message(Action.START_TRANSACTION.value, payload.to_json())
+
+        self.__ws.send(call)
+
+        call_result = self.__ws.receive()
+
+        objects = json.loads(call_result)
+
+        payload = objects[2];
+
+        return payload['transactionId']
+
+    def stop_transaction_req(self, transaction_id, id_tag = None) -> None:
+        '''
+        This method is responsible for notifying to the Central System that the transaction has stopped.
+
+        @param transaction_id This contains the transaction-id as received by the StartTransaction.conf.
+        @param id_tag This contains the identifier which requested to stop the charging. It is optional
+        because a Charge Point may terminate charging without the presence of an idTag, e.g. in case of
+        a reset. A Charge Point SHALL send the idTag if known.
+        '''
+        payload = StopTransactionPayload(
+            meter_stop=1000,
+            timestamp=self.timestamp(),
+            transaction_id=transaction_id,
+            reason=Reason.ev_disconnected.value,
+            id_tag=id_tag
+        )
+
+        call = self.create_call_message(Action.STOP_TRANSACTION.value, payload.to_json())
+
+        self.__ws.send(call)
+
+        self.__ws.receive()
+
+    def meter_values_req(self, connector_id, timestamp, voltage, current, power, soc, transaction_id = None) -> None:
+        '''
+        This method is responsible for sampling the electric meter or other sensor/transducer
+        hardware to provide information about your meter values.
+
+        @param connector_id This contains a number (>0) designating a connector of the
+        Charge Point.‘0’ (zero) is used to designate the main powermeter.
+        @param timestamp Timestamp for measured value(s).
+        @param voltage Represents simulated voltage under a connector of the charging station.
+        @param current Represents simulated current under a connector of the charging station.
+        @param power Represents simulated power under a connector of the charging station.
+        @param soc Represents a percentage of the state of charge of the battery
+        of the vehicle being recharged.
+        @param transaction_id The transaction to which these meter samples are related.
+        '''
+        samples = [
+            SampledValue(
+                value=voltage,
+                context=ReadingContext.transaction_begin.value,
+                format=ValueFormat.raw.value,
+                measurand=Measurand.voltage.value,
+                phase=Phase.l1.value,
+                location=Location.cable.value,
+                unit=Unit.v.value
+            ),
+            SampledValue(
+                value=current,
+                context=ReadingContext.transaction_begin.value,
+                format=ValueFormat.signed_data.value,
+                measurand=Measurand.current_import.value,
+                phase=Phase.l1.value,
+                location=Location.cable.value,
+                unit=Unit.a.value
+            ),
+            SampledValue(
+                value=power,
+                context=ReadingContext.transaction_begin.value,
+                format=ValueFormat.raw.value,
+                measurand=Measurand.power_active_import.value,
+                phase=Phase.l1.value,
+                location=Location.cable.value,
+                unit=Unit.w.value
+            ),
+            SampledValue(
+                value=soc,
+                context=ReadingContext.transaction_begin.value,
+                format=ValueFormat.raw.value,
+                measurand=Measurand.soc.value,
+                phase=Phase.l1.value,
+                location=Location.ev.value,
+                unit=Unit.percent.value
+            )
+        ]
+
+        meter_values = [
+            MeterValue(timestamp, samples)
+        ]
+
+        payload = MeterValuesPayload(connector_id, transaction_id, meter_values)
+
+        call = self.create_call_message(Action.METER_VALUES.value, payload.to_json())
 
         self.__ws.send(call)
 
